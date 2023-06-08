31660be4950e334a0aba0a5ba453de94bf2feb6f
Cardinal4 <126611815+Cardinal4@users.noreply.github.com>
2023-06-08 11:42:56 -0500

    Actualizaciones señores, actualizaciones

--- polinomios.py
+++ polinomios.py
@@ -1,6 +1,6 @@
 from functools import cmp_to_key
 
-def lex(monomial_1, monomial_2): #COMPARA monomial_1 >= monomial_2
+def lex(monomial_1: tuple, monomial_2: tuple):
     for i in range(len(monomial_1)):
         if monomial_1[i] > monomial_2[i]:
             return True
@@ -9,7 +9,7 @@ def lex(monomial_1, monomial_2): #COMPARA monomial_1 >= monomial_2
 
     return True
 
-def grlex(monomial_1, monomial_2):
+def grlex(monomial_1: tuple, monomial_2: tuple):
     sum_1 = sum(monomial_1)
     sum_2 = sum(monomial_2)
 
@@ -32,7 +32,7 @@ class ZTuple:
 
     def __add__(self, z_tuple):
         if len(self.tuple) != len(z_tuple.tuple):
-            raise IndexError('The tuples has not the same length')
+            raise IndexError('The tuples have not the same length')
 
         new_tuple = tuple([self.tuple[_] + z_tuple.tuple[_]
                            for _ in range(len(self.tuple))])
@@ -40,12 +40,18 @@ class ZTuple:
         return ZTuple(new_tuple)
 
     def __eq__(self, z_tuple):
+        if type(self) != type(z_tuple):
+            raise TypeError(type(z_tuple), 'is not comparable with ZTuple')
+
         return self.tuple == z_tuple.tuple
 
     def __hash__(self):
         return self.tuple.__hash__()
 
     def __gt__(self, z_tuple):
+        if type(self) != type(z_tuple):
+            raise TypeError(type(z_tuple), 'is not comparable with ZTuple')
+
         return self.order(self.tuple, z_tuple.tuple)
 
     def __iter__(self):
@@ -65,9 +71,12 @@ class Term:
         self.variables = variables
         self.order = order
 
-    def __eq__(self, o):
-        return self.monomial_tuple == o.monomial_tuple\
-            and self.coefficient == o.coefficient
+    def __eq__(self, term):
+        if type(self) != type(term):
+            raise TypeError(type(term), 'is not comparable with Term')
+
+        return self.monomial_tuple == term.monomial_tuple\
+            and self.coefficient == term.coefficient
 
     def __str__(self):
         if self.is_constant():
@@ -87,14 +96,20 @@ class Term:
         
         return string_representation
 
-    def __gt__(self, o):
-        self.order(self.monomial_tuple, o.monomial_tuple)
+    def __gt__(self, term):
+        if type(self) != type(term):
+            raise TypeError(type(term), 'is not comparable with Term')
 
-    def __mul__(self, o):
-        new_coeff = self.coefficient * o.coefficient
-        new_tuple = self.monomial_tuple + o.monomial_tuple
+        self.order(self.monomial_tuple, term.monomial_tuple)
 
-        return Term(new_coeff, new_tuple, self.variables, self.order)
+    def __mul__(self, term):
+        if type(self) != type(term):
+            raise TypeError(type(term), 'is not comparable with Term')
+
+        new_coeff = self.coefficient * term.coefficient
+        new_tuple = self.monomial_tuple + term.monomial_tuple
+
+        return Term(new_coeff, new_tuple.tuple, self.variables, self.order)
 
     def is_constant(self):
         zero_tuple = ZTuple(tuple([0] * len(self.monomial_tuple)),
@@ -103,31 +118,30 @@ class Term:
         return zero_tuple == self.monomial_tuple
 
 class Polynomial:
-    def __init__(self, terms, variables, order = grlex):
+    def __init__(self, terms: list[Term], variables: list[str], order = grlex):
         self.terms = terms
         self.variables = variables
         self.order = order
         self.clean()
 
-    def clean(self): #HAY QUE PROBAR ESTE MÉTODO!!!
+    def clean(self):
         monomials = set([term.monomial_tuple for term in self.terms])
         new_terms = []
 
         for monomial in monomials:
-            sum_coeff = 0
+            sum_coef = 0
 
             for term in self.terms:
                 if term.monomial_tuple == monomial:
-                    sum_coeff += term.coefficient
+                    sum_coef += term.coefficient
 
-            if sum_coeff != 0:
-                new_terms.append(Term(sum_coeff, monomial.tuple, self.variables))
+            if sum_coef != 0:
+                new_terms.append(Term(sum_coef, monomial.tuple, self.variables))
 
         self.terms = new_terms
         self.sort()
 
     def __str__(self):
-        #self.clean()
         string_representation = ''
 
         for term in self.terms:
@@ -135,24 +149,28 @@ class Polynomial:
 
         return string_representation[0 : -2]
 
-    def __add__(self, o):
-        h = Polynomial(self.terms + o.terms, self.variables, self.order)
+    def __add__(self, poly):
+        if type(self) != type(poly):
+            raise TypeError(type(poly), 'is not comparable with Poynomial')
+
+        h = Polynomial(self.terms + poly.terms, self.variables, self.order)
         h.clean()
 
         return h
 
-    def __mul__(self, o):
+    def __mul__(self, poly):
+        if type(self) != type(poly):
+            raise TypeError(type(poly), 'is not comparable with Polynomial')
+
         h = Polynomial([term1 * term2 for term1 in self.terms
-                        for term2 in o.terms], self.variables, self.order)
+                        for term2 in poly.terms], self.variables, self.order)
         h.clean()
 
         return h
 
     @staticmethod
     def compare(order):
-
         def inner(term_1, term_2):
-
             monomial_1 = term_1.monomial_tuple
             monomial_2 = term_2.monomial_tuple
 
@@ -192,7 +210,6 @@ t2 = Term(1, (1,), ['x'])
 t3 = Term(-1, (0,), ['x'])
 t4 = Term(1, (1,), ['x'])
 
-#f = Polynomial([t1, t2], ['x'])
-#g = Polynomial([t3, t4], ['x'])
 f = Polynomial([t1, t2], ['x'])
-print(f)
+g = Polynomial([t3, t4, t3], ['x'])
+print(f * g)
